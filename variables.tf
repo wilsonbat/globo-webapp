e66f87e209a7ee5038f3ec44d8266f8c36aeafc7
vivek muthuvelu <vivek.muthuvelu@gmail.com>
2025-07-01 09:43:57 -0500

    Switch to using tfe_outputs-8

--- variables.tf
+++ variables.tf
@@ -37,16 +37,16 @@ variable "api_key" {
   description = "(Required) API key for web app to talk to SaaS platform."
 }
 
-variable "vpc_id" {
-  type        = string
-  description = "(Required) VPC ID of VPC for application deployment."
+# variable "vpc_id" {
+#   type        = string
+#   description = "(Required) VPC ID of VPC for application deployment."
 
-}
+# }
 
-variable "public_subnets" {
-  type        = list(string)
-  description = "(Required) List of subnet IDs for EC2 instance deployments."
-}
+# variable "public_subnets" {
+#   type        = list(string)
+#   description = "(Required) List of subnet IDs for EC2 instance deployments."
+# }
 
 # TFE Variables
 
