d10c531dbdeb1044260cb07b6eb22045762b7360
vivek muthuvelu <vivek.muthuvelu@gmail.com>
2025-07-01 14:22:35 -0500

    Switch to using secrets manager

--- resources.tf
+++ resources.tf
@@ -25,11 +25,17 @@ locals {
 # RESOURCES
 ##################################################################################
 
+resource "aws_iam_instance_profile" "main" {
+  name = "${local.name_prefix}-webapp"
+  role = var.ec2_role_name
+  tags = local.common_tags
+}
 resource "aws_instance" "main" {
-  count         = length(data.tfe_outputs.networking.nonsensitive_values.public_subnets)
-  ami           = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
-  instance_type = var.instance_type
-  subnet_id     = data.tfe_outputs.networking.nonsensitive_values.public_subnets[count.index]
+  count                = length(data.tfe_outputs.networking.nonsensitive_values.public_subnets)
+  ami                  = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
+  iam_instance_profile = aws_iam_instance_profile.main.name
+  instance_type        = var.instance_type
+  subnet_id            = data.tfe_outputs.networking.nonsensitive_values.public_subnets[count.index]
   vpc_security_group_ids = [
     aws_security_group.webapp_http_inbound_sg.id,
     aws_security_group.webapp_ssh_inbound_sg.id,
@@ -68,34 +74,12 @@ resource "aws_instance" "main" {
 
   user_data = templatefile("./templates/userdata.sh", {
     playbook_repository = var.playbook_repository
+    secret_id           = var.api_key_secret_id
+    host_list_ssm_name  = local.host_list_ssm_name
+    site_name_ssm_name  = local.site_name_ssm_name
   })
 }
 
-resource "null_resource" "webapp" {
-
-  triggers = {
-    webapp_server_count = length(aws_instance.main.*.id)
-    web_server_names    = join(",", aws_instance.main.*.id)
-  }
-
-  provisioner "file" {
-    content = templatefile("./templates/application.config.tpl", {
-      hosts     = aws_instance.main.*.private_dns
-      site_name = "${local.name_prefix}-taco-wagon"
-      api_key   = var.api_key
-    })
-    destination = "/home/ec2-user/application.config"
-  }
-
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    port        = "22"
-    host        = aws_instance.main[0].public_ip
-    private_key = module.ssh_keys.private_key_openssh
-  }
-
-}
 
 resource "aws_lb" "main" {
   name               = "${local.name_prefix}-webapp"
